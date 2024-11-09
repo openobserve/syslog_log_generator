022743d17d2d72d807b1d10ddf303f628dcd0cd8
Prabhat Sharma <hi.prabhat@gmail.com>
2024-11-09 12:34:36 -0800

    1000 events

--- generate_logs.sh
+++ generate_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python syslog_gen.py --host 127.0.0.1 --port 5514 --file sample_logs.txt --count 100000
+python syslog_gen.py --host 127.0.0.1 --port 5514 --file sample_logs.txt --count 1000
